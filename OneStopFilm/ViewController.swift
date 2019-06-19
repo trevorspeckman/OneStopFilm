b1518ea7748fb0a1d01a83b7d5d7d372bcbd947b
Trevor Speckman <trevorsp23@gmail.com>
2019-06-19 13:06:29 -0700

    Initial Commit

---
+++ OneStopFilm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OneStopFilm
+//
+//  Created by Trevor Speckman on 6/19/19.
+//  Copyright © 2019 Trevor Speckman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
